0f355f1803c8d1c9b62a46f78b1c8dc53f10b19a
muzkii <andriyo.averill@ui.ac.id>
2024-09-11 11:28:32 +0700

    Tutorial 2 commit

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5&2j_nv^z5d=cg352ugep3$4opdle$cfkpl^*z$%02-7-7k^x^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "andriyo-averill-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost".pbp.cs.ui, "127.0.0.1", "andriyo-averill-mentalhealthtrackerNEW.ac.id"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://pbp.cs.ui.ac.id/andriyo.averill/mentalhealthtrackerNEW", "http://pbp.cs.ui.ac.id/andriyo.averill/mentalhealthtrackerNEW"]
